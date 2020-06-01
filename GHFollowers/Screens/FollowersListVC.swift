98b9e866c40e9d483bd520b19d541d9a7042fd53
Francis Acquaah <francisk994@gmail.com>
2020-06-01 00:12:30 +0000

    completed  collection view empty state view

--- GHFollowers/Screens/FollowersListVC.swift
+++ GHFollowers/Screens/FollowersListVC.swift
@@ -44,13 +44,20 @@ class FollowersListVC: UIViewController {
     
     
     func getFollowers(username: String,page: Int){
-        
+        showLoadingView()
         NetworkManager.shared.getFollowers(for: username, page: page) { [weak self] result in
             guard let self = self else{ return }
+            self.dismissLoadingView()
             switch result{
                 case.success(let followers):
                     if followers.count < 100 {self.hasMoreFollowers = false}
                     self.followers.append(contentsOf:followers)
+                    
+                    if self.followers.isEmpty{
+                        let message = "This user doesn't have any followers. Go follow them 😁."
+                        self.showEmptyStateView(with: message, in: self.view)
+                        return
+                    }
                     self.updateData()
                 case.failure(let error):
                     self.presentGFAlertOnMainThread(title: "Bad stuff", message: error.rawValue, buttonTitle: "ok")
